971599cef56f038a3715130e99b68643893b743f
Aleksanderdir <79590679+Aleksanderdir@users.noreply.github.com>
2022-05-23 15:50:11 +0300

    3

---
+++ lib/lib/CenterListWidget.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:task_list/flavor.dart';
+import 'package:task_list/fluversCrud.dart';
+import 'package:task_list/list_page.dart';
+import 'package:task_list/repoData.dart';
+
+class CenterListWidget extends StatefulWidget {
+  const CenterListWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CenterListWidget> createState() => _CenterListWidgetState();
+}
+
+class _CenterListWidgetState extends State<CenterListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    return Card(
+      child: Padding(padding: const EdgeInsets.all(8.0),
+
+        child: Column(
+          children: [
+            Card(
+              child: Row(
+
+                  children: <Widget>[
+                    const Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left:16.0),
+                        child: Text('Tasks',
+                            //  textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.w800,
+                            )),
+                      ),
+                    ),
+                    IconButton(
+                      iconSize: 54.0,
+                      splashRadius: 32.0,
+                      onPressed: () {
+                        context.read<RepoData>().setVisible(true);
+                      },
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                  ]),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            VisibleWidget(controller: controller),
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: ListPage(),
+
+                  /*
+                   ListView(
+                    children: _buildList(),
+                  ),  */
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _loadItemTasks() async {
+    fluversCrud crud = fluversCrud();
+    List<Flavor> list = await crud.getAll();
+    context.read<RepoData>().setFlavor(list);
+  }
+
+  @override
+  void initState() {
+    fluversCrud crud = fluversCrud();
+    crud.init();
+
+    super.initState();
+    _loadItemTasks();
+  }
+}
+
+class VisibleWidget extends StatelessWidget {
+  VisibleWidget({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+
+    void _onChange(){
+      context.read<RepoData>().setErrorText(false);
+
+
+    }
+    return Visibility(
+
+
+      replacement: const SizedBox.shrink(),
+      // maintainSize: false,
+      // maintainAnimation: true,
+      visible: context.watch<RepoData>().getVisible,
+      child: TextField(onChanged:(String a){
+        _onChange();
+
+      } ,
+textInputAction: TextInputAction.done,
+        keyboardAppearance:Brightness.light,
+        enabled: true,
+        autofocus: true,
+        controller: controller,
+        onEditingComplete: () async {
+          if (controller.text == null || controller.text == '') {
+            context.read<RepoData>().setErrorText(true);
+
+          } else {
+            context.read<RepoData>().setErrorText(false);
+            fluversCrud crud = fluversCrud();
+            crud.init();
+            // int id = await  crud.add(controller.text);
+            Flavor flavor = Flavor(name: controller.text, id: 0);
+            context.read<RepoData>().addFlavor(flavor);
+            context.read<RepoData>().setVisible(false);
+            _onChange();
+          controller.clear();
+
+          }
+          /*
+          ItemTaskCrud crud = ItemTaskCrud();
+          int id = await crud.add(controller.text);
+          ItemTask itemTask = ItemTask.create(id, controller.text, false);
+          context.read<RepoData>().addTask(itemTask);
+           */
+        },
+        decoration: InputDecoration( labelText: 'Введите имя задачи',
+
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(
+                    color: context.read<RepoData>().getErrorText
+                        ? Colors.red
+                        : Colors.blueAccent,
+                    width: 1.0)),
+
+          errorText: context.read<RepoData>().getErrorText
+              ? 'Поле ввода не может быть пустым'
+              : null),
+        // decoration: const InputDecoration(
+        //   hintText: '',
+      ),
+    );
+  }
+}
---
+++ lib/lib/repoData.dart
@@ -0,0 +1,60 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:task_list/ItemTask.dart';
+import 'package:task_list/flavor.dart';
+
+class RepoData with ChangeNotifier, DiagnosticableTreeMixin {
+  // Provider.of<RepoData>(context, listen: false);
+  bool _visible = false;
+  List<ItemTask> _list = [];
+  List<Flavor> _listFlavor = [];
+  bool ErrorText =false;
+  void setErrorText(bool _ErrorText) {
+    ErrorText = _ErrorText;
+    notifyListeners();
+  }
+  bool get getErrorText => ErrorText;
+  List<Flavor> get getFlavors => _listFlavor;
+
+  void addFlavor(Flavor flavor) {
+    _listFlavor.add(flavor);
+    notifyListeners();
+  }
+
+  bool get getVisible => _visible;
+  List<ItemTask> get getTaskList => _list;
+
+  void setVisible(bool _visible1) {
+    _visible = _visible1;
+    notifyListeners();
+  }
+
+  void setTasksList(List<ItemTask> _list1) {
+    _list = _list1;
+    notifyListeners();
+  }
+void setFlavor(List<Flavor> _listflavor ){
+  _listFlavor = _listflavor;
+  notifyListeners();
+}
+
+  void addTask(ItemTask itemTask) {
+    _list.add(itemTask);
+    notifyListeners();
+  }
+
+  void delTask(int id) {
+    _list.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty(
+        '_visible', _visible.toString()) /*IntProperty('count', count)*/);
+  }
+}
